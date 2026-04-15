544da87f88d1ca1431205083853fddda2b2a9da9
SteffVill <a.villalba.soles@gmail.com>
2026-04-15 14:05:41 -0400

    Proyecto simple: Registro de creyentes

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-10 text-center">
+      <h1 className="text-4xl font-bold uppercase">¡Tailwind funcionando!</h1>
+    </div>
+  )
+}
+export default App
